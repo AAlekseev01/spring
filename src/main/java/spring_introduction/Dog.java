47f9d3d0c28fbfbd77bfd6eeb76aeeb3da09e2d4
leksor <leksor01@Wdwdfwf.ru>
2025-06-17 15:39:40 +0300

    ну вот так

--- src/main/java/spring_introduction/Dog.java
+++ src/main/java/spring_introduction/Dog.java
@@ -1,12 +1,32 @@
 package spring_introduction;
 
 public class Dog implements Pet {
-    public Dog (){
+    private String name;
+
+    public Dog() {
         System.out.println("Dog bean is created");
+
     }
+
     @Override
-    public void say () {
+    public void say() {
         System.out.println("Bow-Wow");
+
+//        public String getName() {
+//            return name;
+//        }
+//
+//        public void setName (String name){
+//            this.name = name;
+//        }
+    }
+    public void init() {
+        System.out.println("Class Dog : init method");
+    }
+    public void destroy() {
+        System.out.println("Class Dog : destroy method");
+
     }
 
 }
+
